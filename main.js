5be3272450a7ea45da18cd0483046ba0b34f5873
H4k3r <108434180+Br1am@users.noreply.github.com>
2022-11-25 17:00:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log(" projeto no console");
+ 
+            var firstName = "Ranbir";
+            var lastName = "Kapoor";
+ 
+            var fullName = firstName.concat(lastName);
+            console.log(fullName);
+            var a = 9;
+            var b = 4;
+            var add = a+b;
+            console.log(add)
+            var sub = a-b
+            console.log(sub)
+            var mult = a*b
+            console.log(mult)
+            var divi = a/b
+            console.log(divi)
